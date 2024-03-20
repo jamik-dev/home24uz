14584b2045d72f9e8ef94e406bc979c099d126a2
jamik-dev <jamik.oik.group@gmail.com>
2024-03-20 10:42:22 +0500

    basic setup done

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-layout>
+    <TheHeader />
+    <Nuxt/>
+    <TheFooter />
+  </a-layout>
+</template>
+<script>
+import TheHeader from "@/components/global/TheHeader.vue";
+import TheFooter from "@/components/global/TheFooter.vue";
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+<style>
+  
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 class="text-6xl text-red-500">Index Page</h1>
+    <a-button type="primary">
+      Primary
+    </a-button>
   </div>
 </template>
 
