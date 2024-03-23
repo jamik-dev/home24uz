3a6a234bb2870ae43e883c9f3b26b45846c4aafd
jamik-dev <jamik.oik.group@gmail.com>
2024-03-23 23:13:32 +0500

    cart page building ~ 1

--- components/custom/product/Seen.vue
+++ components/custom/product/Seen.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="w-full grid grid-cols-12 gap-6 mt-6">
       <customCartDefault v-for="item in 6" :key="item"
-        :data="{ img: require(`~/assets/img/chair/2.png`), text: 'Руководительское кресло Metta Комплект 5.1 (Чёрный)', discount: 0, price: '3 512 750', price_old: '3 512 750', rating: '5.0' }" />
+        :data="{ img: require(`~/assets/img/chair/1.png`), text: 'Руководительское кресло Metta Комплект 5.1 (Чёрный)', discount: 0, price: '3 512 750', price_old: '3 512 750', rating: '5.0' }" />
     </div>
   </section>
 </template>
