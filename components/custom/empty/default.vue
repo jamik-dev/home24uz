bac80a621bfa6d64d61663c0ff1bf4c6eac2b036
jamik-dev <jamik.oik.group@gmail.com>
2024-03-23 23:49:55 +0500

    404 and 500 pages done

--- components/custom/empty/default.vue
+++ components/custom/empty/default.vue
@@ -2,14 +2,14 @@
   <div class="w-full flex items-center justify-center py-7">
     <div class="flex items-center gap-6 flex-col">
       <slot name="image"></slot>
-      <h2 class="font-ttfirs text-4xl">
+      <h2 class="font-ttfirs text-4xl max-w-[60%] text-center">
         <slot name="title"></slot>
       </h2>
       <p class="text-center font-ttfirs text-grey-text text-lg max-w-[60%]">
         <slot name="description"></slot>
       </p>
       <button @click="$router.push('/')"
-        class="py-4 w-[350px] rounded-lg border border-orange text-orange font-medium text-lg">На главную</button>
+        class="py-4 hover:text-white hover:bg-orange duration-200 w-[350px] rounded-lg border border-orange text-orange font-medium text-lg">На главную</button>
     </div>
   </div>
 </template>
---
+++ pages/_.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <div class="w-full px-16 py-[60px]">
+      <customEmptyDefault>
+        <template #image>
+          <img src="~/assets/icon/404.svg" alt="404 page not found">
+        </template>
+        <template #title>
+          Страница не найдена!
+        </template>
+        <template #description>
+          Не переживайте и продолжайте покупку
+        </template>
+      </customEmptyDefault>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
