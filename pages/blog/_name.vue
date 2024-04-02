e691179fd3672bfef3a59ea96d599344d942c1b6
jamik-dev <jamik.oik.group@gmail.com>
2024-04-02 13:06:38 +0500

    blog api built

--- pages/blog/_name.vue
+++ pages/blog/_name.vue
@@ -6,51 +6,34 @@
       <div class="w-full grid grid-cols-5 gap-12 mt-10">
         <div class="col-span-4">
           <div>
-            <h2 class="font-ttfirs font-medium text-3xl max-w-[90%] leading-10">Любовь к семье и верность университету -
-              по
-              наследству: два поколения преподавателей Грехнёвых в ННГУ</h2>
-            <p class="text-grey-text mt-2">4 июля</p>
+            <h2 class="font-ttfirs font-medium text-3xl max-w-[90%] leading-10">{{ post.title }}</h2>
+            <p class="text-grey-text mt-2">{{ datePosted }}</p>
           </div>
           <div class="mt-6">
-            <p class="text-grey-text text-lg">Ежегодному празднованию Международного женского дня положила начало Вторая
-              Международная конференция
-              социалисток, состоявшаяся в Копенгагене в 1910 году. Праздновать этот день предложила Клара Цеткин[3].
-              Одной
-              из целей была обозначена борьба за всеобщее избирательное право для женщин.
-              <br />
-              <br />
-              Предложение получило единодушную поддержку более 100 женщин из 17 стран, однако дата празднования
-              зафиксирована не была[2]. До 1914 года в разных странах этот день отмечали в различные числа марта[3].
-            </p>
+            <p class="text-grey-text text-lg" v-html="post.desc"></p>
           </div>
           <div class="w-full h-[400px] rounded-3xl overflow-hidden mt-10">
-            <img class="w-full h-full object-cover" src="~/assets/img/default/4.png" alt="blog img">
-          </div>
-          <div class="mt-10">
-            <h3 class="font-ttfirs font-medium text-xl leading-8 max-w-[90%]">Теперь вернемся к теме подарка.</h3>
-            <p class="text-grey-text mt-6 text-lg max-w-[90%]">Международному женскому дню предшествовал Национальный
-              женский день, который отметили в США 28 февраля 1909 года в память о событиях предыдущего, 1908 года, —
-              забастовке работниц текстильной промышленности в Нью-Йорке, которые требовали улучшения условий труда[2].
-            </p>
+            <img class="w-full h-full object-cover" :src="post.lg_img || require(`~/assets/img/default/4.png`)" :alt="post.title">
           </div>
         </div>
         <div class="col-span-1">
           <h3 class="font-ttfirs font-medium text-3xl">Реклама</h3>
-          <div class="space-y-6 mt-10 rounded-2xl overflow-hidden">
-            <img v-for="item in 3" :key="item" :src="require(`~/assets/img/banner/sub/${item}.png`)" alt="ad">
+          <div class="space-y-6 mt-10">
+            <img class="rounded-2xl" v-for="(banner, index) in banners.data" :key="banner.id" :src="banner.lg_img || require(`~/assets/img/banner/sub/${index}.png`)" :alt="banner.type">
           </div>
         </div>
       </div>
       <div class="mt-[120px]">
         <h2 class="text-4xl font-ttfirs">Другие блоги</h2>
         <div class="w-full grid grid-cols-12 gap-6 mt-10">
-          <div v-for="item in 4" :key="item"
-            class="h-[270px] col-span-3 relative rounded-xl overflow-hidden bg-center bg-no-repeat bg-cover group w-full fit-content bg-[url(~/assets/img/blog/1.png)]">
+          <div v-for="post in other_posts" :key="post.id"
+            :style="{ backgroundImage: `url(${post?.lg_img || require(`~/assets/img/blog/1.png`)})` }"
+            class="h-[270px] col-span-3 relative rounded-xl overflow-hidden bg-center bg-no-repeat bg-cover group w-full fit-content">
             <div
               class="absolute h-full w-full top-0 left-0 bg-gradient-to-t from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0)] flex items-end p-4">
-              <nuxt-link to="/blog/8-marta"
+              <nuxt-link :to="`/blog/${post.slug}`"
                 class="font-semibold text-xl text-white max-w-[90%] group-hover:text-orange-2 duration-200 cursor-pointer">
-                Поздравляем с ”8 марта” любимых женщин</nuxt-link>
+                {{ post.title }}</nuxt-link>
             </div>
           </div>
         </div>
@@ -59,17 +42,35 @@
   </main>
 </template>
 <script>
+import {mapGetters} from 'vuex';
 export default {
+  async asyncData({store, params}) {
+    await store.dispatch('posts/getPost', params.name);
+    await store.dispatch('banners/getBanners', {type: '', limit: 3});
+  },
   data: () => {
     return {
       breadCrumb: [
         { name: 'Главная', url: '/' },
         { name: 'Блог', url: '/blog' }
       ],
+      months: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+    }
+  },
+  computed: {
+    ...mapGetters({
+      post: 'posts/post',
+      other_posts: 'posts/other_posts',
+      banners: 'banners/banners',
+    }),
+    datePosted() {
+      const day = new Date(this.post.created_at).getDate();
+      const month = this.months[new Date(this.post.created_at).getMonth()];
+      return `${day} ${month}`;
     }
   },
   mounted() {
-    this.breadCrumb.push({ url: '/blog/' + this.$route.params.name, name: 'Блог 8 марта' })
+    this.breadCrumb.push({ url: '/blog/' + this.$route.params.name, name: this.post.title })
   },
   methods: {
     itemRender(_, type, originalElement) {
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,24 +4,29 @@
       <customBreadCrumb :breadCrumbData="breadCrumb" />
       <h1 class="font-medium font-ttfirs text-4xl mt-4">Блог</h1>
       <div class="w-full grid grid-cols-12 gap-6 mt-14">
-        <div v-for="item in 16" :key="item"
-          class="h-[270px] col-span-3 relative rounded-xl overflow-hidden bg-center bg-no-repeat bg-cover group w-full fit-content bg-[url(~/assets/img/blog/1.png)]">
+        <div v-for="post in posts.data" :key="post.id"
+          :style="{ backgroundImage: `url(${post?.lg_img || require(`~/assets/img/blog/1.png`)})` }"
+          class="h-[270px] col-span-3 relative rounded-xl overflow-hidden bg-center bg-no-repeat bg-cover group w-full fit-content">
           <div
             class="absolute h-full w-full top-0 left-0 bg-gradient-to-t from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0)] flex items-end p-4">
-            <nuxt-link to="/blog/8-marta"
+            <nuxt-link :to="`/blog/${post.slug}`"
               class="font-semibold text-xl text-white max-w-[90%] group-hover:text-orange-2 duration-200 cursor-pointer">
-              Поздравляем с ”8 марта” любимых женщин</nuxt-link>
+              {{ post.title }}</nuxt-link>
           </div>
         </div>
       </div>
-      <div class="w-full mt-16">
-        <a-pagination :total="500" :item-render="itemRender" />
+      <div v-if="posts.last_page > 0" class="w-full mt-16">
+        <a-pagination :total="posts.last_page" :item-render="itemRender" />
       </div>
     </section>
   </main>
 </template>
 <script>
+import {mapGetters} from 'vuex';
 export default {
+  async asyncData({store}) {
+    await store.dispatch('posts/getPosts', {limit: 0});
+  },
   data: () => {
     return {
       breadCrumb: [
@@ -29,6 +34,11 @@ export default {
         { name: 'Блог', url: '/blog' }
       ],
     }
+  },
+  computed: {
+    ...mapGetters({
+      posts: 'posts/posts',
+    })
   },
   methods: {
     itemRender(_, type, originalElement) {
